67c90c2c4a6a7651608b0e7f1f27c54299a01e36
Samvel <loonohod8989@yandex.ru>
2024-05-02 15:46:35 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// alert("Hello");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt ("Вам есть уже 18?", "18");
+// console.log(answer +5);
+
+const answers = [];
+
+answers[0] = prompt("как ваше имя?", "");
+answers[1] = prompt("как ваше фамилия?", "");
+answers[2] = prompt("какой ваш возраст?", "");
+
+document.write(answers);
